8c490e029e5e179cb46d7c7094932d969793136b
hydro <hydro@Hydro>
2015-12-04 09:12:42 +0800

    update seuif97

--- Linux64/seuif97.py
+++ Linux64/seuif97.py
@@ -1,45 +1,116 @@
 
 # -*- coding: utf-8 -*- 
 
-from ctypes import *
-import os 
-# 参数为生成的.so文件所在的绝对路径
-    flib= cdll.LoadLibrary(os.getcwd() + '/SEUIF97.so')
-    
+from ctypes import c_int,c_double,WINFUNCTYPE,windll
+prototype=WINFUNCTYPE(c_double,c_double,c_double,c_int)
+
+def pt(p,t,w): 
+    f=prototype(("seupt",windll.seuif97),)
+    result=f(p,t,w)
+    return result
+
 def pt2h(p,t): 
-    f=flib.seupt
-    f.argtypes = [c_double,c_double,c_int]
-    f.restype = c_double 
+    f=prototype(("seupt",windll.seuif97),)
     result=f(p,t,4)
     return result
 
 def pt2s(p,t): 
-    f=flib.seupt
-    f.argtypes = [c_double,c_double,c_int]
-    f.restype = c_double 
+    f=prototype(("seupt",windll.seuif97),)
     result=f(p,t,5)
     return result
 
-def ph2s(p,h): 
-    f=flib.seuph
-    f.argtypes = [c_double,c_double,c_int]
-    f.restype = c_double 
-    result=f(p,h,4)
+def ph(p,h,w): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,w)
     return result
 
-def ishd(p1,t1,p2): 
-    f=flib.seuishd
-    f.argtypes = [c_double,c_double,c_double]
-    f.restype = c_double 
+def ph2t(p,h): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,1)
+    return result
+
+def ph2s(p,h): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,5)
+    return result
+
+def ph(p,h,w): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,w)
+    return result
+
+
+def ps(p,s,w): 
+    f=prototype(("seups",windll.seuif97),)
+    result=f(p,s,w)
+    return result
+
+def ps2h(p,s): 
+    f=prototype(("seups",windll.seuif97),)
+    result=f(p,s,4)
+    return result
+
+def ps2t(p,s): 
+    f=prototype(("seups",windll.seuif97),)
+    result=f(p,s,1)
+    return result
+
+
+def pv(p,v,w): 
+    f=prototype(("seupv",windll.seuif97),)
+    result=f(p,v,w)
+    return result
+
+def px(p,x,w): 
+    f=prototype(("seupx",windll.seuif97),)
+    result=f(p,x,w)
+    return result
+
+def th(t,h,w): 
+    f=prototype(("seuth",windll.seuif97),)
+    result=f(t,h,w)
+    return result
+
+
+def th2p(t,h): 
+    f=prototype(("seuth",windll.seuif97),)
+    result=f(t,h,0)
+    return result
+
+def th2s(t,h,w): 
+    f=prototype(("seuth",windll.seuif97),)
+    result=f(t,h,5)
+    return result
+
+def ts(t,s,w): 
+    f=prototype(("seuts",windll.seuif97),)
+    result=f(t,s,w)
+    return result
+
+def ts2h(t,s): 
+    f=prototype(("seuts",windll.seuif97),)
+    result=f(t,s,4)
+    return result
+
+def ts2p(t,s): 
+    f=prototype(("seuts",windll.seuif97),)
+    result=f(t,s,0)
+    return result
+
+def tv(t,v,w): 
+    f=prototype(("seutv",windll.seuif97),)
+    result=f(t,v,w)
+    return result
+
+def ishd(p1,t1,p2):
+    ishdprototype=WINFUNCTYPE(c_double,c_double,c_double,c_double)
+    f=ishdprototype(("seuishd",windll.seuif97),)
     result=f(p1,t1,p2)
     return result
 
-def ief(p1,t1,p2,t2): 
-    f=flib.seuief
-    f.argtypes = [c_double,c_double,c_double,c_double]
-    f.restype = c_double 
+def ief(p1,t1,p2,t2):
+    iefprototype=WINFUNCTYPE(c_double,c_double,c_double,c_double,c_double)
+    f=iefprototype(("seuief",windll.seuif97),)
     result=f(p1,t1,p2,t2)
     return result
 
-h=pt2h(16.1,535.2)
-print(h)
--- Windows32/seuif97.py
+++ Windows32/seuif97.py
@@ -24,6 +24,22 @@ def ph(p,h,w):
     result=f(p,h,w)
     return result
 
+def ph2t(p,h): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,0)
+    return result
+
+def ph2s(p,h): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,5)
+    return result
+
+def ph(p,h,w): 
+    f=prototype(("seuph",windll.seuif97),)
+    result=f(p,h,w)
+    return result
+
+
 def ps(p,s,w): 
     f=prototype(("seups",windll.seuif97),)
     result=f(p,s,w)
@@ -33,6 +49,12 @@ def ps2h(p,s):
     f=prototype(("seups",windll.seuif97),)
     result=f(p,s,4)
     return result
+
+def ps2t(p,s): 
+    f=prototype(("seups",windll.seuif97),)
+    result=f(p,s,1)
+    return result
+
 
 def pv(p,v,w): 
     f=prototype(("seupv",windll.seuif97),)
@@ -49,6 +71,17 @@ def th(t,h,w):
     result=f(t,h,w)
     return result
 
+
+def th2p(t,h): 
+    f=prototype(("seuth",windll.seuif97),)
+    result=f(t,h,0)
+    return result
+
+def th2s(t,h,w): 
+    f=prototype(("seuth",windll.seuif97),)
+    result=f(t,h,5)
+    return result
+
 def ts(t,s,w): 
     f=prototype(("seuts",windll.seuif97),)
     result=f(t,s,w)
@@ -57,6 +90,11 @@ def ts(t,s,w):
 def ts2h(t,s): 
     f=prototype(("seuts",windll.seuif97),)
     result=f(t,s,4)
+    return result
+
+def ts2p(t,s): 
+    f=prototype(("seuts",windll.seuif97),)
+    result=f(t,s,0)
     return result
 
 def tv(t,v,w): 
