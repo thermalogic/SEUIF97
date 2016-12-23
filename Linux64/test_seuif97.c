a1c4485a9d7991ffc3abe6743b7d67a306eecf13
Cheng Maohua <cmh@seu.edu.cn>
2016-12-23 20:01:36 +0800

    update seuif97

---
+++ Linux64/test_seuif97.c
@@ -0,0 +1,15 @@
+#include <stdio.h>   
+#include "seuif97.h"
+
+int main(){
+
+      double p=16.13;
+      double t=535;
+      double h;
+
+      h=seupt(p,t,4);
+      printf("%5.2f",h);
+
+      return 0;
+ }
+
