8fbc916b357200d4d7f2673b85b83c7ea6d049e1
Cheng Maohua <cmh@seu.edu.cn>
2017-01-20 11:44:31 +0800

    update M1

--- Windows64/test_seuif97.py
+++ Windows64/test_seuif97.py
@@ -1,30 +1,13 @@
 # -*- coding: utf-8 -*- 
 
 import seuif97
-import timeit
-import cProfile, pstats
-import io
 
 p=16.10
 t=535.10
 
-#t = timeit.Timer("seuif97.pt2h(p,t)","from __main__ import seuif97,p,t")  
-#if97time=t.timeit(1)
-#print('Time(s)=',if97time)
-#print(h)
-
-pr = cProfile.Profile()
-pr.enable()
 
 h=seuif97.pt2h(p,t)
     
-pr.disable()
-s = io.StringIO()
-sortby = 'cumulative'
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print (s.getvalue())
-
 
 print(h)
 
