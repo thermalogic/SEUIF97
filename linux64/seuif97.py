f4a6c31e9f9308463a01fdfcfd2dd847218a9fde
thermalogic <cmhnj@189.cn>
2017-03-22 02:08:32 +0800

    Linux

--- linux64/seuif97.py
+++ linux64/seuif97.py
@@ -38,6 +38,104 @@ def ph2t(p, h):
     result = f(p, h, 1)
     return result
 
+def ps2t(p, s):
+    f = flib.seups
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, s, 1)
+    return result
+
+def ps2h(p, s):
+    f = flib.seups
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, s, 4)
+    return result
+
+def ps2v(p, s):
+    f = flib.seups
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, s, 3)
+    return result
+
+def ps2x(p, s):
+    f = flib.seups
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, s, 15)
+    return result
+
+
+def hs2t(h, s):
+    f = flib.seuhs
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(h, s, 1)
+    return result
+
+def hs2p(h, s):
+    f = flib.seuhs
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(h, s, 0)
+    return result
+
+def hs2v(h, s):
+    f = flib.seuhs
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(h, s, 3)
+    return result
+
+def hs2x(h, s):
+    f = flib.seuhs
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(h, s, 15)
+    return result
+
+def px2t(p, x):
+    f = flib.seupx
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, x, 1)
+    return result
+
+def px2h(p, x):
+    f = flib.seupx
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, x, 4)
+    return result
+
+def px2s(p, x):
+    f = flib.seupx
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, x, 5)
+    return result
+
+def px2v(p, x):
+    f = flib.seupx
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(p, x, 3)
+    return result
+
+def tx2p(t, x):
+    f = flib.seutx
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(t, x, 0)
+    return result
+
+def tx2v(t, x):
+    f = flib.seutx
+    f.argtypes = [c_double, c_double, c_int]
+    f.restype = c_double
+    result = f(t, x, 3)
+    return result
 
 def tx2h(t, x):
     f = flib.seutx
