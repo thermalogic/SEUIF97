42d796d4b35357c918b51daa4ddb2ba7df165056
thermalogic <cmh@seu.edu.cn>
2018-06-01 00:36:14 +0800

    update utf8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="seuif97",
-    version="1.0.0.1.1",
+    version="1.0.0.1.2",
     author="Cheng Maohua",
     author_email="cmh@seu.edu.cn",
     url="https://github.com/PySEE/SEUIF97",
@@ -10,7 +10,7 @@ setuptools.setup(
     package_data={'': ['*.so','*.dll','README.md']},
     license="MIT",
     description='IAPWS-IF97 high-speed shared library(Windows64,Linux64) in ANSI C,Python API',
-    long_description=open("./seuif97/README.md", "r").read(),   
+    long_description=open("./seuif97/README.md", "r",encoding="utf8").read(),   
     platforms=["Windows64","Linux64"],
     classifiers=[
         "Programming Language :: Python :: 3",
