de2141deda3408a8030d21739c8e8ac549c303a2
thermalogic <cmh@seu.edu.cn>
2018-06-01 00:13:23 +0800

    update README.md for PyPI

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="seuif97",
-    version="1.0.0.0.9",
+    version="1.0.0.1.1",
     author="Cheng Maohua",
     author_email="cmh@seu.edu.cn",
     url="https://github.com/PySEE/SEUIF97",
     packages=['seuif97'],
-    package_data={'': ['*.so','*.dll','LDESC4PyPI.md']},
+    package_data={'': ['*.so','*.dll','README.md']},
     license="MIT",
     description='IAPWS-IF97 high-speed shared library(Windows64,Linux64) in ANSI C,Python API',
-    long_description=open("./seuif97/LDESC4PyPI.md", "r").read(),   
+    long_description=open("./seuif97/README.md", "r").read(),   
     platforms=["Windows64","Linux64"],
     classifiers=[
         "Programming Language :: Python :: 3",
