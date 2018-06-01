0eeb240ad04b8e2a094f3786659f565707f3fb91
thermalogic <cmh@seu.edu.cn>
2018-06-02 01:29:38 +0800

    update README.rst

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="seuif97",
-    version="1.0.0.1.4",
+    version="1.0.0.1.5",
     author="Cheng Maohua",
     author_email="cmh@seu.edu.cn",
     url="https://github.com/PySEE/SEUIF97",
     packages=['seuif97'],
-    package_data={'': ['*.so','*.dll','README.md']},
+    package_data={'': ['*.so','*.dll','README.rst']},
     license="MIT",
     description="IAPWS-IF97 high-speed shared library(Windows64,Linux64) in ANSI C,Python API",
-    long_description=open("./seuif97/README.md", "r",encoding="utf8").read(),   
+    long_description=open("./seuif97/README.rst", "r",encoding="utf8").read(),   
     platforms=["Windows64","Linux64"],
     classifiers=[
         "Programming Language :: Python :: 3",
