4490003e82b1a68ba9aead4b5544c795d341e161
thermalogic <cmh@seu.edu.cn>
2019-01-07 19:03:47 +0800

    cleanup

--- demo-python/demo_seuif97.py
+++ demo-python/demo_seuif97.py
@@ -1,19 +1,31 @@
-# -*- coding: utf-8 -*-
 
+"""
+The Simple example of seuif97.py
+
+License: this code is in the public domain
+
+Author:   Cheng Maohua
+Email:    cmh@seu.edu.cn
+
+Last modified: 2018.11.28
+
+"""
 import seuif97
 
-p=16.10
-t=535.10
+p = 16.10
+t = 535.10
 # type 1 : ??2?(in1,in2)
-h=seuif97.pt2h(p,t)
-s=seuif97.pt2s(p,t)
-v=seuif97.pt2v(p,t)
+h = seuif97.pt2h(p, t)
+s = seuif97.pt2s(p, t)
+v = seuif97.pt2v(p, t)
 
-print("(p,t),h,s,v:","{:>.2f} \t {:>.2f} \t {:>.2f} \t {:>.3f} \t {:>.4f}".format(p, t, h, s, v))
+print("(p,t),h,s,v:",
+      "{:>.2f} \t {:>.2f} \t {:>.2f} \t {:>.3f} \t {:>.4f}".format(p, t, h, s, v))
 
 # type 2: ??(in1,in2,outid)
-t=seuif97.ph(p,h,1)
-s=seuif97.ph(p,h,5)
-v=seuif97.ph(p,t,3)
+t = seuif97.ph(p, h, 1)
+s = seuif97.ph(p, h, 5)
+v = seuif97.ph(p, t, 3)
 
-print("(p,h),t,s,v:","{:>.2f} \t {:>.2f} \t {:>.2f} \t {:>.3f} \t {:>.4f}".format(p, h, t, s, v))
+print("(p,h),t,s,v:",
+      "{:>.2f} \t {:>.2f} \t {:>.2f} \t {:>.3f} \t {:>.4f}".format(p, h, t, s, v))
