a644a189670e7ebee3061b26b33b9e6220def8a1
thermalogic <cmh@seu.edu.cn>
2019-10-23 23:50:18 +0800

    update Dependencies:

--- demo-csharp/demo_seuif97.cs
+++ demo-csharp/demo_seuif97.cs
@@ -1,37 +1,58 @@
 /*
-SEUIF97 Library:
-    1 C:/Windows/system/libseuif97.dll
-    2 seuif97.cs
 
-you may use the C# compiler and set the Platform target of application to the same as the dynamic library to build the demo application
+-------------- Windows ---------------
 
-1  Choose the C# compiler
+Dependencies:
+------
+    1 SEUIF97 for Windows: C:/Windows/system/libseuif97.dll
+    2 API for C#: seuif97.cs
 
-    1.1 The C#5 compiler in Windows 
+Building the C# Application Using SEUIF97：
+-------
+
+you may use the C# compiler and set the Platform target of application to the same as the dynamic library to build the C# Application Using SEUIF97
+
+1  C# compiler
+
+you may us the C#5 compiler in Windows or Install the latest C# compiler
+
+1.1 Using the C# compiler in Windows 
        
-        Add the path of C#5 compiler C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ to the system environment variables Path 
+    Add the path of C# compiler,for example C:\Windows\Microsoft.NET\Framework64\v4.0.30319\ to the system environment variables #Path# 
 
-    1.2 The latest C# compiler 
+1.2 Install and use the latest C# compiler 
 
-       1.2.1 Using nuget to install the latest version of  C# compiler (https://github.com/dotnet/roslyn)
+    1.2.1 Using #nuget# to install the latest C# compiler (https://github.com/dotnet/roslyn)
 
-         1) mkdir C:\csharp\ for the latest version of  C# compiler,then download nuget to the path
+        1) mkdir C:\csharp\ for the latest C# compiler,then download #nuget# to the path
 
-         2）In the path C:\csharp\, install the latest release without Visual Studio, run one of the following `nuget` command lines:
+        2）In the path C:\csharp\, install the latest release without Visual Studio, run one of the following `nuget` command lines:
 
            >nuget install Microsoft.Net.Compilers   # Install C# and VB compilers
            >nuget install Microsoft.CodeAnalysis    # Install Language APIs and Services
 
-           then the compiler is installed in the path  C:\csharp\Microsoft.Net.Compilers.2.10.0\tools\
+        then the compiler is installed in the path, for example  C:\csharp\Microsoft.Net.Compilers.2.10.0\tools\
 
-       1.2.2 add the path C:\csharp\Microsoft.Net.Compilers.2.10.0\tools\ to the system environment variables Path  
+    1.2.2 add the path C:\csharp\Microsoft.Net.Compilers.2.10.0\tools\ to the system environment variables #Path#  
 
 2. Build on x64 libseuif97.dll：
 
+
+
   >csc -out:demo.exe demo_seuif97.cs seuif97.cs /platform:"x64"
 
-Run:
-./demo
+3. Run:
+
+  >./demo
+
+------------- Linux  -------------------
+
+Dependencies:
+------
+    1 SEUIF97 for Linux64: /usr/lib/libseuif97.so
+    2 API for C#: seuif97.cs
+
+-----------------------------------------
 
 License: this code is in the public domain
 
