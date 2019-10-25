f4266f1c1d4e1aadb8f9582674a30274e6b53e35
thermalogic <cmh@seu.edu.cn>
2019-10-25 15:59:25 +0800

    remove suffix

--- demo-csharp/seuif97.cs
+++ demo-csharp/seuif97.cs
@@ -16,29 +16,29 @@ namespace seuif97
 {
     public class Seuif97
     {
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seupt(double p, double t, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seuph(double p, double h, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seups(double p, double s, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seupv(double p, double v, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seuth(double t, double h, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seuts(double t, double s, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seutv(double t, double v, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seuhs(double t, double s, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seupx(double p, double x, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seutx(double t, double x, int wid);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seuishd(double pi, double ti, double pe);
-        [DllImport("libseuif97.dll", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("libseuif97", CallingConvention = CallingConvention.StdCall)]
         public static extern double seuief(double pi, double ti, double pe, double te);
 
     }
