4543abc76ddc1bda5cc3dcf74c0f40b1c282b08f
thermalogic <cmh@seu.edu.cn>
2021-01-19 13:53:06 +0800

    cleanup

--- demo/demo-rust/src/main.rs
+++ demo/demo-rust/src/main.rs
@@ -2,8 +2,9 @@ mod seuif97;
 use seuif97::pt;
 
 fn main() {
-    let mut h:f64=0.0;
-    h=pt(16.0,535.0,4);
-    println!("{}",h);
-  
+    let p: f64 = 16.0;
+    let t: f64 = 535.9;
+    let h: f64 = pt(p, t, 4);
+    let s: f64 = pt(p, t, 5);
+    println!("({},{}),{} {}", p, t, h, s);
 }
